31f0338647bb12a625b166b54dd3c56ebe4ba5ab
Monique Robinson <monique.robinson2@gmail.com>
2018-02-15 18:40:30 -0500

    Pseudo Code

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// Name Variables for counter, user score, random number and crystal values
+
+// write a loop to pick a random number from an array
+
+// write a code to pick a random number for each crystal
+
+// if user score equals the random number the player wins and game resets
+// else keep counting
+// if user guess is higher than random number player loses and game resets
