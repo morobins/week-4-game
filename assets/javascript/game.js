3557badfeb97a63d62f786e4577d2d550bcaa38b
Monique Robinson <monique.robinson2@gmail.com>
2018-02-15 21:35:50 -0500

    added reset funtion

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,6 @@
-// Name Variables for counter, user score, random number and crystal values
+$('.document').ready(function () {
+
+  // Name Variables for counter, user score, random number and crystal values
 var randomNumber = Math.floor(Math.random() * 120) + 19;
 console.log(randomNumber);
 var currentValue = 0;
@@ -10,14 +12,42 @@ var diamond = Math.floor(Math.random() * 12) + 1;
 console.log(diamond);
 var pinkGem = Math.floor(Math.random() * 12) + 1;
 console.log(pinkGem);
+var wins = 0
+var loses = 0
 
-$
+// reset game 
+var reset = function () {
+currentValue = 0;
+randomNumber = Math.floor(Math.random() * 120) + 19;
+redGem = Math.floor(Math.random() * 12) + 1;
+blueGem = Math.floor(Math.random() * 12) + 1;
+diamond = Math.floor(Math.random() * 12) + 1;
+pinkGem = Math.floor(Math.random() * 12) + 1;
+}
 
+//set random number
+$('#random-number').text(randomNumber);
 
-// write a loop to pick a random number from an array
+//on click add value to currentValue
+$("#blue-gem").on("click", function() {
+// var currentValue + blueGem
+});
 
-// write a code to pick a random number for each crystal
-
+$('#diamond').on('click', )
+$('#pink-gem').on('click', )
+$('#red-gem').on('click', )
 // if user score equals the random number the player wins and game resets
+if (currentValue === randomNumber) {
+  wins++
+} else if (currentValue < randomNumber) {
+  //keep counting
+};
+
+// if (currentValue > randomNumber) {
+//   loses==
+// };
+
 // else keep counting
 // if user guess is higher than random number player loses and game resets
+
+});
