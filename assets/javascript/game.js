09e2d7438a1954e4ae7422e9f0cbf581c0e96e47
Monique Robinson <monique.robinson2@gmail.com>
2018-02-16 14:41:19 -0500

    Got the clicks to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,14 +4,17 @@ $('.document').ready(function () {
 var randomNumber = Math.floor(Math.random() * 120) + 19;
 console.log(randomNumber);
 var currentValue = 0;
+// var gems = Math.floor(Math.random() * 12) + 1;
 var redGem = Math.floor(Math.random() * 12) + 1;
-console.log(redGem);
+// console.log(redGem);
 var blueGem = Math.floor(Math.random() * 12) + 1;
-console.log(blueGem);
+// console.log(blueGem);
 var greenGem = Math.floor(Math.random() * 12) + 1;
-console.log(greenGem);
+// console.log(greenGem);
 var pinkGem = Math.floor(Math.random() * 12) + 1;
-console.log(pinkGem);
+// console.log(pinkGem);
+var gems = [redGem, blueGem, greenGem, pinkGem];
+console.log(gems);
 var wins = 0
 var loses = 0
 
@@ -19,35 +22,52 @@ var loses = 0
 var reset = function () {
 currentValue = 0;
 randomNumber = Math.floor(Math.random() * 120) + 19;
-redGem = Math.floor(Math.random() * 12) + 1;
-blueGem = Math.floor(Math.random() * 12) + 1;
-greenGem = Math.floor(Math.random() * 12) + 1;
-pinkGem = Math.floor(Math.random() * 12) + 1;
+gems = Math.floor(Math.random() * 12) + 1;
+// redGem = Math.floor(Math.random() * 12) + 1;
+// blueGem = Math.floor(Math.random() * 12) + 1;
+// greenGem = Math.floor(Math.random() * 12) + 1;
+// pinkGem = Math.floor(Math.random() * 12) + 1;
 }
 
-//set random number
+//set text numbers
 $('#random-number').text(randomNumber);
+$('#wins').text(wins);
+$('#loses').text(loses);
 
-//on click add value to currentValue
-$(blueGem).text($(this).attr("data-letter"));
-// var crystalValue = ($(this).attr("data-crystalvalue"));
-$('.crystals').on("click", function() {
-// var currentValue + blueGem
+
+// run for loop to a value into each crystal
+for (i = 0; i < gems.length; i++) {
+  var gemsOptions = $("img").html(gems[i]);
+  // gemsOptions.addClass("gemStones");
+ 
+  gemsOptions.attr("data-gems", gems[i]);
+  gemsOptions.text(gems[i]);
+  // console.log(gems[i]);
+  $("#crystals").append(gemsOptions);
+};
+
+// var gems = $('.crystals img');
+// $(gems).text($(this).attr("data-letter"));
+
+//on click add crystalValue to currentValue
+$('#crystals').on("click", function() {
+  var crystalValue = ($(this).attr("data-gems"));
+  crystalValue = parseInt(crystalValue);
+  currentValue += crystalValue;
+  $('#current-value').text(currentValue);
+  alert(crystalValue);
 });
 
-$('#green-gem').on('click', )
-$('#pink-gem').on('click', )
-$('#red-gem').on('click', )
 // if user score equals the random number the player wins and game resets
 if (currentValue === randomNumber) {
   wins++
 } else if (currentValue < randomNumber) {
   //keep counting
-};
+}
 
-// if (currentValue > randomNumber) {
-//   loses==
-// };
+if (currentValue > randomNumber) {
+  loses--;
+}
 
 // else keep counting
 // if user guess is higher than random number player loses and game resets
